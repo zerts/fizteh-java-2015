30419eb703ad313d6c83308f23937de72a338c47
Andrey <zertsalov96@yandex.ru>
2015-10-12 12:12:49 +0300

    New changelist

---
+++ zerts/src/test/java/ru/fizteh/fivt/students/zerts/moduletests/library/GeoParserTest.java
@@ -0,0 +1,38 @@
+package ru.fizteh.fivt.students.zerts.moduletests.library;
+
+import javafx.beans.binding.When;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import static javafx.beans.binding.Bindings.when;
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GeoParserTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception{
+        URL getCityName = mock(URL.class);
+
+    }
+
+    @Test
+    public void testGetMyPlace() throws Exception {
+
+    }
+
+    public void testGetCoordinates() throws Exception {
+
+    }
+
+    public void testNear() throws Exception {
+
+    }
+}
---
+++ zerts/src/test/java/ru/fizteh/fivt/students/zerts/moduletests/library/TimeParserTest.java
@@ -0,0 +1,13 @@
+package ru.fizteh.fivt.students.zerts.moduletests.library;
+
+import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TimeParserTest extends TestCase {
+
+    public void testPrintGoneDate() throws Exception {
+
+    }
+}
