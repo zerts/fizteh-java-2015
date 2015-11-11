56a9a695add5457c6783c69fb156c3fe6bb6d3bb
Andrey <zertsalov96@yandex.ru>
2015-11-11 03:02:23 +0300

    Few tests added

---
+++ zerts/src/test/java/ru/fizteh/fivt/students/zerts/collectionquery/impl/FromStmtTest.java
@@ -0,0 +1,21 @@
+package ru.fizteh.fivt.students.zerts.collectionquery.impl;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by User on 11.11.2015.
+ */
+public class FromStmtTest extends TestCase {
+
+    public void testFrom() throws Exception {
+
+    }
+
+    public void testSelect() throws Exception {
+
+    }
+
+    public void testSelectDistinct() throws Exception {
+
+    }
+}
