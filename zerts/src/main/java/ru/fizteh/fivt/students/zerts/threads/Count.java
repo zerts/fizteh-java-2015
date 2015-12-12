79dce2e14abf06b5d443385748b6ba5a737216dc
Andrey <zertsalov96@yandex.ru>
2015-12-12 10:31:55 +0300

    Revert "Counter added"
    This reverts commit 0edb0ac9b5a94c3afacea4256459f99baf27d373.

--- zerts/src/main/java/ru/fizteh/fivt/students/zerts/threads/Count.java
+++ zerts/src/main/java/ru/fizteh/fivt/students/zerts/threads/Count.java
@@ -1,40 +1,7 @@
 package ru.fizteh.fivt.students.zerts.threads;
 
 /**
- * Created by zerts on 03.12.2015.
+ * Created by User on 03.12.2015.
  */
 public class Count {
-    private static volatile int currentId;
-    private static volatile int numberOfThreads;
-    private static class PrintThread extends Thread {
-        private int id;
-        PrintThread(int id) {
-            this.id = id;
-        }
-        @Override
-        public void run() {
-            while (true) {
-                if (currentId == this.id) {
-                    System.out.println("Thread-" + this.id);
-                    currentId = (currentId + 1) % numberOfThreads;
-                    /*try {
-                        Thread.sleep(TimeUnit.SECONDS.toMillis(1L));
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }*/
-                } else {
-                    Thread.yield();
-                }
-            }
-        }
-    }
-
-    public static void main(String[] argv) {
-        numberOfThreads = new Integer(argv[0]);
-        currentId = 0;
-        for (int i = 0; i < numberOfThreads; i++) {
-            PrintThread thread = new PrintThread(i);
-            thread.start();
-        }
-    }
 }
