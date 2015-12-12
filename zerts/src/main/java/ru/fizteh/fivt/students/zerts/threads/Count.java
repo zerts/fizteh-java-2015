1432ed16abbc0e1965a222c5b211f28473254ba6
Andrey <zertsalov96@yandex.ru>
2015-12-12 10:25:32 +0300

    Revert "Counter fixed"
    This reverts commit c3c925d304fce2977d196a8319343f74e6d541e7.

--- zerts/src/main/java/ru/fizteh/fivt/students/zerts/threads/Count.java
+++ zerts/src/main/java/ru/fizteh/fivt/students/zerts/threads/Count.java
@@ -4,51 +4,37 @@ package ru.fizteh.fivt.students.zerts.threads;
  * Created by zerts on 03.12.2015.
  */
 public class Count {
-
-    private static class Caller {
-
-        private int currentId = 0;
-
-        private class PrintThread implements Runnable {
-            private int numberOfThreads;
-            private int id;
-            PrintThread(int id, int numberOfThreads) {
-                this.id = id;
-                this.numberOfThreads = numberOfThreads;
-            }
-
-            @Override
-            public void run() {
-                while (true) {
-                    synchronized (Caller.this) {
-                        while (currentId != this.id) {
-                            try {
-                                Caller.this.wait();
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        System.out.println("Thread-" + this.id);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        currentId = (currentId + 1) % numberOfThreads;
-                        Caller.this.notifyAll();
-                    }
-                }
-            }
+    private static volatile int currentId;
+    private static volatile int numberOfThreads;
+    private static class PrintThread extends Thread {
+        private int id;
+        PrintThread(int id) {
+            this.id = id;
         }
-
-        public void startCount(int numberOfThreads) {
-            for (int i = 0; i < numberOfThreads; i++) {
-                (new Thread(new PrintThread(i, numberOfThreads))).start();
+        @Override
+        public void run() {
+            while (true) {
+                if (currentId == this.id) {
+                    System.out.println("Thread-" + this.id);
+                    currentId = (currentId + 1) % numberOfThreads;
+                    /*try {
+                        Thread.sleep(TimeUnit.SECONDS.toMillis(1L));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }*/
+                } else {
+                    Thread.yield();
+                }
             }
         }
     }
 
     public static void main(String[] argv) {
-        (new Caller()).startCount(new Integer(argv[0]));
+        numberOfThreads = new Integer(argv[0]);
+        currentId = 0;
+        for (int i = 0; i < numberOfThreads; i++) {
+            PrintThread thread = new PrintThread(i);
+            thread.start();
+        }
     }
 }
